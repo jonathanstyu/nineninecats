9d9cf8d7c0e9078fe75938692568e512d35ff42e
App Academy Student <student@appacademy.io>
2013-06-05 14:36:58 -0700

    Cat rental approval done

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -7,7 +7,17 @@ class CatRentalRequest < ActiveRecord::Base
   validates_with RequestValidator
 
   def approve
+    self.status = 'approved'
 
+    self.cat.cat_rental_requests.each do |request|
+      next if request == self
+      if self.begin_date < request.end_date &&
+        self.end_date > request.begin_date
+        request.status = 'denied'
+        request.save!
+      end
+    end
+    self.save!
   end
 
 
